4977599b928ddf6ad205186b366f331c0c519eac
ogi <orgertaabazi@gmail.com>
2024-11-27 22:06:05 +0000

    update

--- main.tf
+++ main.tf
@@ -2,6 +2,6 @@ provider "aws" {
   region = "eu-west-2" # London region
 }
 
-resource "aws_s3_bucket" "devsecops-project0" {
-  bucket = "devsecops-project0"
+resource "aws_s3_bucket" "devsecopsproject0" {
+  bucket = "devsecopsproject0"
 }
